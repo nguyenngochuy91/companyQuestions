a6e47bb7431c7a7668dbeeef80789b313883539f
nguyenngochuy91 <huyn@cvm6h4zv52.cvm.iastate.edu>
2019-09-19 01:11:41 -0500

    add subsum

--- facebook/subarraySum.py
+++ facebook/subarraySum.py
@@ -4,11 +4,19 @@ Created on Thu Sep 19 01:05:34 2019
 
 @author: huyn
 """
-
+import random
 #560. Subarray Sum Equals K
 #Given an array of integers and an integer k, you need to find the total number
 # of continuous subarrays whose sum equals to k.
 def subarraySum( nums, k) -> int:
     count = 0
     return count
-    
+
+def generateTest(n):
+    arr = []
+    for i in range(n):
+        arr.append(random.randint(-10,10))
+    return arr
+
+arr,k = generateTest(100),random.randint(1,100)
+print (arr,k)
