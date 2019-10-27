5d9fb6bf07ffc84d09d800648f785a6333996692
Huy <huyn@iastate.edu>
2019-10-27 01:19:15 -0500

    push

---
+++ facebook/onsite/burstBalloon.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 25 17:29:59 2019
+
+@author: Huy Nguyen
+"""
+
+#312. Burst Balloons
+#Given n balloons, indexed from 0 to n-1. Each balloon is painted with a number on
+# it represented by array nums. You are asked to burst all the balloons. If the you 
+# burst balloon i you will get nums[left] * nums[i] * nums[right] coins. Here left and 
+# right are adjacent indices of i. After the burst, the left and right then becomes adjacent.
+#
+#Find the maximum coins you can collect by bursting the balloons wisely.
+from typing import List
+class Solution:
+    def maxCoins(self, nums: List[int]) -> int:
+        return
+        
+nums = [3,1,5,8]
